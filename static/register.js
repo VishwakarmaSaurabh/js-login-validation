6c96fcab1d7523220aa25787a97dae28d3e9c1f2
VishwakarmaSaurabh <321saurabhv@gmail.com>
2022-01-24 00:47:08 +0530

    changed error showing logic on registration page

--- static/register.js
+++ static/register.js
@@ -6,6 +6,13 @@ const username = document.querySelector("#username");
 const password = document.querySelector("#password");
 const passwordConfirm = document.querySelector("#password_confirm");
 const form = document.querySelector("#register_form");
+const errorBlock = document.querySelectorAll(".error");
+
+const errorFullname = document.querySelector(".errorFullname");
+const errorUsername = document.querySelector(".errorUsername");
+const errorEmail = document.querySelector(".errorEmail");
+const errorPassword = document.querySelector(".errorPassword");
+const errorCPassword = document.querySelector(".errorCPassword");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -18,18 +25,52 @@ form.addEventListener("submit", (e) => {
     passwordConfirm.value
   );
 
-  var user = {
-    'fullname': fullname.value,
-    'username': username.value,
-    'email': email.value,
-    'password': password.value,
-  };
+  if (Object.keys(errors).length > 0) {
+    if (errors["fullname"]) {
+      errorFullname.innerHTML = errors["fullname"];
+      errorBlock[0].style.visibility = "visible";
+    }
+    if (errors["email"]) {
+      errorEmail.innerHTML = errors["email"];
+      errorBlock[1].style.visibility = "visible";
+    }
+    if (errors["username"]) {
+      errorUsername.innerHTML = errors["username"];
+      errorBlock[2].style.visibility = "visible";
+    }
+    if (errors["password"]) {
+      errorPassword.innerHTML = errors["password"];
+      errorBlock[3].style.visibility = "visible";
+    }
+    if (errors["passwordConfirm"]) {
+      errorCPassword.innerHTML = errors["passwordConfirm"];
+      errorBlock[4].style.visibility = "visible";
+    }
 
-  if (Object.keys(errors).length === 0) {
-    localStorage.setItem(username.value, JSON.stringify(user));
+    setTimeout(() => {
+      errorBlock.forEach((element) => (element.style.visibility = "hidden"));
+    }, 5000);
+  } else {
 
-    location.href = "./index.html";
+    var user = {
+      fullname: fullname.value,
+      username: username.value,
+      email: email.value,
+      password: password.value,
+    };
+
+    const userCheck = localStorage.getItem(user.username);
+    const userCheckParsed = JSON.parse(userCheck);
+    if (userCheckParsed != null) {
+      errors["username"] = "Username already exists.";
+    } else {
+      localStorage.setItem(username.value, JSON.stringify(user));
+      location.href = "./index.html";
+    }
   }
+
+
+  console.log(errors);
 
   /* var userFromStorage = localStorage.getItem("saurabh@gmail.com");
   var userOut = JSON.parse(userFromStorage);
